63a1186bbc2cc2dd08226639a099a7275ac8983c
WBrasher <60417570+WBrasher@users.noreply.github.com>
2020-06-08 15:04:02 -0500

    Add files via upload

--- src/fb2.js
+++ src/fb2.js
@@ -1,19 +1,19 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/storage";
 
 // Your web app's Firebase configuration
-var config = {
-  apiKey: "AIzaSyAmUxTq1pAYEFYHC8PytBrBMMa8z0dP62Q",
-  authDomain: "gradtracker-361db.firebaseapp.com",
-  databaseURL: "https://gradtracker-361db.firebaseio.com",
-  projectId: "gradtracker-361db",
-  storageBucket: "gradtracker-361db.appspot.com",
-  messagingSenderId: "312349194926",
-  appId: "1:312349194926:web:c0f280c7059486fab1cb3f",
-  measurementId: "G-QC4X1FW79T",
+const firebaseConfig = {
+  apiKey: "AIzaSyCR35NLabbpyIzpr_-tpXn8z8xlcj2aAoQ",
+  authDomain: "test-6912a.firebaseapp.com",
+  databaseURL: "https://test-6912a.firebaseio.com",
+  projectId: "test-6912a",
+  storageBucket: "test-6912a.appspot.com",
+  messagingSenderId: "705460843359",
+  appId: "1:705460843359:web:10ee2ff551e58a6dd9bcea",
 };
 // Initialize Firebase
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 //firebase.analytics();
 const db = firebase.firestore();
 db.settings({ timestampsInSnapshots: true });
